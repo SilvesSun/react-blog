d7ec5b308428867717b59bfe0d3a809c8d5d383d
sksun <sunshengkai27@gmail.com>
2019-05-03 17:21:08 +0800

    简单的小屏幕样式

--- src/components/DefaultLayout/DefaultLayout.js
+++ src/components/DefaultLayout/DefaultLayout.js
@@ -1,8 +1,7 @@
 import React,{Component} from 'react';
-import HeadNav from '../HeadNav/HeadNav';
 import PostDetail from "../Posts/PostDetail";
 import SidebarLeft from "../SidebarLeft/SidebarLeft";
-import {Col, Row} from "antd";
+import {Button, Col, Icon, Layout, Menu, Row} from "antd";
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import SidebarRight from "../SidebarRight/SidebarRight";
 import MainContent from "../MainComponent/MainContent";
@@ -12,6 +11,9 @@ import SearchRes from "../SearchRes/SearchRes";
 import Archive from "../Archive/Archive";
 import '../base.less'
 
+
+const MenuItemGroup = Menu.ItemGroup;
+
 export default class DefaultLayout extends Component{
   constructor(props){
     super(props);
@@ -19,7 +21,13 @@ export default class DefaultLayout extends Component{
       detail: '',
       typeId: 0,
       searchText: '',
-    }
+      hiddenLeft: 'hidden',
+      hiddenContent: '',
+      hiddenRight: 'hidden',
+      disableLeft: false,
+      disableRight: false,
+      collapsed: true,
+    };
   }
 
   handleTypeChange(e){
@@ -30,17 +38,95 @@ export default class DefaultLayout extends Component{
     );
   }
 
+  handleClickMenu(e){
+    if (e === 'left'){
+      if (this.state.hiddenLeft === 'hidden') {
+          this.setState({
+            hiddenLeft: '',
+            hiddenContent: 'hidden',
+            hiddenRight: 'hidden',
+          })
+        }else {
+          this.setState({
+            hiddenLeft: 'hidden',
+            hiddenContent: '',
+            hiddenRight: 'hidden',
+          })
+        }
+    }else if (e === 'right'){
+      if (this.state.hiddenRight === 'hidden') {
+          this.setState({
+            hiddenLeft: 'hidden',
+            hiddenContent: 'hidden',
+            hiddenRight: '',
+          })
+        }else {
+          this.setState({
+            hiddenLeft: 'hidden',
+            hiddenContent: '',
+            hiddenRight: 'hidden',
+          })
+        }
+    }else {
+      this.setState({
+            hiddenLeft: 'hidden',
+            hiddenContent: '',
+            hiddenRight: 'hidden',
+          })
+    }
+  }
+
   render(){
     return (
-      <div id="DefaultLayout">
-        <HeadNav/>
+      <Layout id="DefaultLayout">
+        <div id="HeadNav">
+          <Row>
+            <Col span={24}>
+              <div className="nav-wrap">
+                <div className="nav-logo-wrap">
+                    诗酒趁年华
+                </div>
+                <div className="nav-list-wrap">
+                  <Menu selectedKeys={[this.state.current]} mode="horizontal" onClick={this.handleClick}>
+                    <Menu.Item key="home">
+                        <a href="/">
+                            首页
+                        </a>
+                    </Menu.Item>
+                    <Menu.Item key="archive">
+                        <a href="/archive/">
+                            归档
+                        </a>
+                    </Menu.Item>
+                    <Menu.SubMenu title={<span><Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} /></span>} className='sidebar-menu'>
+                      <MenuItemGroup>
+                        <Menu.Item className='sidebar-menu-item'>
+                          <Button type="primary" onClick={this.handleClickMenu.bind(this, 'left')} className={'buttonLeft'}>网站信息</Button>
+                        </Menu.Item>
+                        <Menu.Item className='sidebar-menu-item'>
+                          <Button type="primary" onClick={this.handleClickMenu.bind(this, 'content')} className={'buttonLeft'}>文章</Button>
+                        </Menu.Item>
+                        <Menu.Item className='sidebar-menu-item'>
+                          <Button type="primary" onClick={this.handleClickMenu.bind(this, 'right')}  className={'buttonRight'}>分类与搜索</Button>
+                        </Menu.Item>
+                      </MenuItemGroup>
+                    </Menu.SubMenu>
+                  </Menu>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+
         <BrowserRouter>
           <Switch>
           <div className="content-wrap">
             <div id="Home">
               <Row className="main-wrap">
-                <Col span={3} className="sidebar-left"><SidebarLeft history={this.props.history} location={this.props.location}/></Col>
-                <Col span={14} offset={2} id={'main-part'}>
+                <Col span={3} className={this.state.hiddenLeft + " sidebar-left"} >
+                  <SidebarLeft history={this.props.history} location={this.props.location}/>
+                </Col>
+                <Col span={14} offset={2} id={'main-part'} className={this.state.hiddenContent}>
                   <Route  path='/blog/:Id' component={PostDetail} />
                   <Route  path='/type/:Id' component={TypeDetail} />
                   <Route  path='/tag/:Id' component={TagDetail} />
@@ -49,13 +135,13 @@ export default class DefaultLayout extends Component{
                   <Route path="/blogs/" component={MainContent} />
                   <Route exact path="/" component={() => (<Redirect to={`/blogs/`}/>)} />
                 </Col>
-                <Col span={3}><SidebarRight history={this.props.history} handleTypeChange={this.handleTypeChange.bind(this)}/></Col>
+                <Col span={3} className={this.state.hiddenRight + " sidebar-right"}><SidebarRight history={this.props.history} handleTypeChange={this.handleTypeChange.bind(this)}/></Col>
               </Row>
             </div>
           </div>
           </Switch>
         </BrowserRouter>
-      </div>
+      </Layout>
       )
   }
 }
